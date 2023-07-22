ecad43567c963af85c186d83db220307e6d85a7f
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-07-22 23:46:00 +0200

    add endpoints for getting and setting amount

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory, request, jsonify
+import json
 
 app = Flask(__name__, static_folder="../frontend/vue-project/dist")
 
@@ -17,9 +18,28 @@ def serve_js(filename):
 @app.route('/send-cash', methods=['POST'])
 def send_cash():
     data = request.get_json()
-    print(data)
+    with open("./backend/data/data.json", "r") as json_file:
+        json_data = json_file.read()
+    json_data = json.loads(json_data)
+    name = data['name']
+    current = float(json_data[name])
+    amount = float(data['price'])
+    current += amount
+    json_data[name] = current
+    with open("./backend/data/data.json", "w") as json_file:
+        json.dump(json_data, json_file)
     return jsonify({'status': 'success'}), 200
 
+
+@app.route('/get-cash', methods=['GET'])
+def get_amount():
+    with open("./backend/data/data.json", "r") as json_file:
+        json_data = json_file.read()
+    json_data = json.loads(json_data)
+    amount = 0
+    for key in json_data:
+        amount += float(json_data[key])
+    return jsonify({'amount': amount}), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
